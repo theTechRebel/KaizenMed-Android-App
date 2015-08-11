6fcc86a383b8b56c389899582dd6da6c0bf5c202
Steven Dumbura <stevedumbura@gmail.com>
2015-08-11 15:04:41 +0200

    added PushNotifications

--- app/src/main/java/com/afrikaizen/kaizenmed/activities/MainActivity.java
+++ app/src/main/java/com/afrikaizen/kaizenmed/activities/MainActivity.java
@@ -1,6 +1,9 @@
 package com.afrikaizen.kaizenmed.activities;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -8,6 +11,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,9 +26,18 @@ import com.afrikaizen.kaizenmed.rest.API;
 import com.afrikaizen.kaizenmed.rest.ApiService;
 import com.afrikaizen.kaizenmed.singleton.AppBus;
 import com.afrikaizen.kaizenmed.singleton.AppPreferences;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
 import com.squareup.otto.Subscribe;
 
+import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.util.concurrent.TimeoutException;
 
 import retrofit.RestAdapter;
 
@@ -32,7 +45,9 @@ import retrofit.RestAdapter;
  * Created by Steve on 07/08/2015.
  */
 public class MainActivity extends AppCompatActivity implements
-    NavigationView.OnNavigationItemSelectedListener{
+    NavigationView.OnNavigationItemSelectedListener {
+
+
 
     //Defining Variables
     TextView doctorsName, ward;
@@ -41,6 +56,8 @@ public class MainActivity extends AppCompatActivity implements
     private DrawerLayout drawerLayout;
     Menu navigationMenu;
     public static final String ENDPOINT = "http://www.afrikaizen.com/kaizenmed";
+    PushNotifications push;
+    PushNotificationsHandler pushHandler;
 
     @Override
     protected void onResume() {
@@ -55,7 +72,7 @@ public class MainActivity extends AppCompatActivity implements
         AppBus.getInstance().unregister(this);
     }
 
-    private API buildApi(){
+    private API buildApi() {
         return new RestAdapter.Builder()
                 .setEndpoint(ENDPOINT)
                 .build()
@@ -63,9 +80,22 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
+    protected void onStop() {
+        super.onStop();
+        push.interrupt();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set up push notifications result handler
+        pushHandler = new PushNotificationsHandler(this);
+
+        //set up push notifications
+        push = new PushNotifications();
+        push.start();
 
         // Initializing Toolbar and setting it as the actionbar
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -82,7 +112,7 @@ public class MainActivity extends AppCompatActivity implements
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
 
         //initialise the ActionBarDrawerToggle
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer){
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer) {
 
             @Override
             public void onDrawerClosed(View drawerView) {
@@ -104,13 +134,13 @@ public class MainActivity extends AppCompatActivity implements
         //calling sync state is necessay or else your hamburger icon wont show up
         actionBarDrawerToggle.syncState();
 
-        doctorsName = (TextView)findViewById(R.id.username_header);
-        ward = (TextView)findViewById(R.id.ward);
+        doctorsName = (TextView) findViewById(R.id.username_header);
+        ward = (TextView) findViewById(R.id.ward);
 
         doctorsName.setText(AppPreferences.getInstance(this).getDoctorsName());
         ward.setText("Ward Number: " + AppPreferences.getInstance(this).getDoctorsID());
 
-        if(savedInstanceState == null){
+        if (savedInstanceState == null) {
             onNavigationItemSelected(navigationMenu.findItem(R.id.patient_results));
         }
     }
@@ -120,6 +150,10 @@ public class MainActivity extends AppCompatActivity implements
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    private void handlePushNotifications(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -155,38 +189,131 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     public boolean onNavigationItemSelected(MenuItem menuItem) {
 
-            //Checking if the item is in checked state or not, if not make it in checked state
-            if(menuItem.isChecked()) menuItem.setChecked(false);
-            else menuItem.setChecked(true);
+        //Checking if the item is in checked state or not, if not make it in checked state
+        if (menuItem.isChecked()) menuItem.setChecked(false);
+        else menuItem.setChecked(true);
 
-            //Closing drawer on item click
-            drawerLayout.closeDrawers();
+        //Closing drawer on item click
+        drawerLayout.closeDrawers();
         Fragment fragment = new Fragment();
-            //Check to see which item was being clicked and perform appropriate action
-            switch (menuItem.getItemId()){
-                //Replacing the main content with ContentFragment Which is our Inbox View;
-                case R.id.patient_results:
-                    fragment = new PatientResultsFragment();
-                    break;
-                case R.id.patient_history:
-                    Toast.makeText(getApplicationContext(),"Patient History",Toast.LENGTH_SHORT).show();
-                    //fragment = new PatientHistoryFragment();
-                    break;
-                case R.id.notifications:
-                    Toast.makeText(getApplicationContext(),"Notifications",Toast.LENGTH_SHORT).show();
-                    //fragment = new NotificationsFragment();
-                    break;
-                case R.id.my_patients:
-                    Toast.makeText(getApplicationContext(),"My Patients",Toast.LENGTH_SHORT).show();
-                    //fragment = new MyPatientsFragment();
-                    break;
-                default:
-                    break;
-            }
+        //Check to see which item was being clicked and perform appropriate action
+        switch (menuItem.getItemId()) {
+            //Replacing the main content with ContentFragment Which is our Inbox View;
+            case R.id.patient_results:
+                fragment = new PatientResultsFragment();
+                break;
+            case R.id.patient_history:
+                Toast.makeText(getApplicationContext(), "Patient History", Toast.LENGTH_SHORT).show();
+                //fragment = new PatientHistoryFragment();
+                break;
+            case R.id.notifications:
+                Toast.makeText(getApplicationContext(), "Notifications", Toast.LENGTH_SHORT).show();
+                //fragment = new NotificationsFragment();
+                break;
+            case R.id.my_patients:
+                Toast.makeText(getApplicationContext(), "My Patients", Toast.LENGTH_SHORT).show();
+                //fragment = new MyPatientsFragment();
+                break;
+            default:
+                break;
+        }
 
         android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.frame, fragment);
         fragmentTransaction.commit();
         return true;
+    }
+
+
+    private static class PushNotificationsHandler extends Handler {
+
+        private final WeakReference<MainActivity> currentActivity;
+
+        public PushNotificationsHandler(MainActivity activity){
+            currentActivity = new WeakReference<MainActivity>(activity);
         }
+
+        @Override
+        public void handleMessage(Message message){
+            MainActivity activity = currentActivity.get();
+            if (activity!= null){
+                activity.handlePushNotifications(message.getData().getString("result"));
+            }
+        }
+    }
+
+
+    public class PushNotifications extends Thread {
+        Context ctx;
+        private final static String QUEUE_NAME = "hello";
+        Connection connection;
+        Channel channel;
+
+        @Override
+        public void run() {
+            super.run();
+            try {
+                ConnectionFactory factory = new ConnectionFactory();
+                factory.setUsername("admin");
+                factory.setPassword("admin");
+                factory.setVirtualHost("/");
+                factory.setHost("192.168.1.101");
+                factory.setPort(5672);
+                connection = factory.newConnection();
+                channel = connection.createChannel();
+
+                channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+                Bundle msgBundle = new Bundle();
+                msgBundle.putString("result", "waiting for msgz :)");
+                Message msg = new Message();
+                msg.setData(msgBundle);
+                pushHandler.sendMessage(msg);
+
+                Log.d("RabbitFoot", "waiting for msgz :)");
+
+                Consumer consumer = new DefaultConsumer(channel) {
+                    @Override
+                    public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                            throws IOException {
+                        String message = new String(body, "UTF-8");
+
+                        Bundle msgBundle = new Bundle();
+                        msgBundle.putString("result", message);
+                        Message msg = new Message();
+                        msg.setData(msgBundle);
+                        pushHandler.sendMessage(msg);
+
+
+                        Log.d("RabbitFoot", message);
+                    }
+                };
+                channel.basicConsume(QUEUE_NAME, true, consumer);
+            } catch (IOException e) {
+                Log.d("RabbitFoot", e.getCause() + " " + e.getMessage());
+            } catch (TimeoutException e) {
+                Log.d("RabbitFoot", e.getCause() + " " + e.getMessage());
+            }
+
+
+        }
+
+        @Override
+        public void interrupt() {
+            super.interrupt();
+            Log.d("RabbitFoot", "no longer listening");
+
+            try {
+                channel.close();
+                connection.close();
+            } catch (IOException e) {
+                Log.d("RabbitFoot", e.getCause() + " " + e.getMessage());
+            } catch (TimeoutException e) {
+                Log.d("RabbitFoot", e.getCause() + " " + e.getMessage());
+            }
+        }
+
+
+    }
+
 }
